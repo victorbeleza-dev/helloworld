27fab6d7a7a7ee1f74d8a98bf281db8f265b9fc8
fctassinari <ftassinari@yahoo.com.br>
2022-11-08 14:36:05 -0300

    Update HelloApplication.java

--- src/main/java/br/com/smanager/hello/HelloApplication.java
+++ src/main/java/br/com/smanager/hello/HelloApplication.java
@@ -11,7 +11,7 @@ public class HelloApplication {
 
 	@RequestMapping("/")
 	public String home() {
-		return "Hello Mundo!!!!";
+		return "Hello wrld!!!!";
 	}
 
 	public static void main(String[] args) {
