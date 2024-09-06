3087af9f59e34fd272f318693075ab1fd1a82e44
Your Name <you@example.com>
2024-09-06 16:18:48 +0000

    teste

--- src/main/java/br/com/smanager/hello/HelloApplication.java
+++ src/main/java/br/com/smanager/hello/HelloApplication.java
@@ -11,7 +11,7 @@ public class HelloApplication {
 
 	@RequestMapping("/")
 	public String home() {
-		return "Ola Mundoooooooooooooooo!!!";
+		return "Ola Mundo!!!";
 	}
 
 	public static void main(String[] args) {
