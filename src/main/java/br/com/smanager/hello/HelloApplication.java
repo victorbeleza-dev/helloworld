4cd9290d60f24233e0fee1e0d3ea78db41597c4b
Victor Beleza <felipe.beleza29@gmail.com>
2024-09-09 13:39:38 +0000

    teste

--- src/main/java/br/com/smanager/hello/HelloApplication.java
+++ src/main/java/br/com/smanager/hello/HelloApplication.java
@@ -11,7 +11,7 @@ public class HelloApplication {
 
 	@RequestMapping("/")
 	public String home() {
-		return "Ola Mundofdhasdfhasdlkjfhalsdkjj!!!";
+		return "Ola Mundo Teste VB!!!";
 	}
 
 	public static void main(String[] args) {
